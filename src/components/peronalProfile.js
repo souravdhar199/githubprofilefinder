2cec7d2a6050efa8354b2de4a6d98dcbfa999d13
sourav199 <souravdhar1999@gmail.com>
2022-09-20 17:11:31 -0400

    Added few data and learend Countup

--- src/components/peronalProfile.js
+++ src/components/peronalProfile.js
@@ -3,6 +3,8 @@ import { useContext } from "react";
 import GithubContext from "../GithubContext";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import CountUp from "react-countup";
+
 export const PersonalProfile = () => {
   const navs = useNavigate();
   const params = useParams();
@@ -22,7 +24,7 @@ export const PersonalProfile = () => {
         <div className="section1">
           <div className="image">
             {" "}
-            <img src={state.userInfo.avatar_url} />
+            <img src={state.userInfo.avatar_url} alt="" />
           </div>
           <div className="you">
             <h1>
@@ -31,14 +33,55 @@ export const PersonalProfile = () => {
                 ? state.userInfo.login
                 : state.userInfo.name}
             </h1>
-            <p>{state.userInfo.bio}</p>
+            <p className="bio">{state.userInfo.bio}</p>
 
             <button>
-              <a href={state.userInfo.html_url}>Take me to</a>
+              <a
+                href={state.userInfo.html_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Profile
+              </a>
             </button>
           </div>
+        </div>
+
+        <div className="section2">
+          <p>
+            Followers:{" "}
+            <>
+              <CountUp
+                start={0}
+                end={state.userInfo.followers}
+                delay={1}
+              ></CountUp>
+            </>
+          </p>
+          <p>
+            Repo's:{" "}
+            <>
+              <CountUp
+                start={0}
+                end={state.userInfo.public_repos}
+                delay={1}
+              ></CountUp>
+            </>
+          </p>
+
+          <p>
+            Gist's:{" "}
+            <>
+              <CountUp
+                start={0}
+                end={state.userInfo.public_gists}
+                delay={1}
+              ></CountUp>
+            </>
+          </p>
         </div>
       </div>
     </>
   );
 };
+//public_repos  public_gists
